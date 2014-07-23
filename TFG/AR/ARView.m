97b917abfaf30d49ee3957b6023d195eee546d8d
Tovkal <@BTApgI5Dif7fEQ$Go8#Zo8z4n*TsK>
2014-07-23 21:53:16 +0200

    Handeling rotation of the view.

--- TFG/AR/ARView.m
+++ TFG/AR/ARView.m
@@ -74,6 +74,75 @@
     self.captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
     [self.layer addSublayer: self.captureVideoPreviewLayer];
     [self.captureSession startRunning];
+	
+	[[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationChanged:) name:@"UIDeviceOrientationDidChangeNotification" object:nil];
+}
+
+- (void)orientationChanged:(NSNotification *)notification
+{
+	CGRect bounds = CGRectMake(0, 0, 1, 1);
+    
+    int xInt = 0;
+    int yInt = 0;
+	
+	CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+	
+	xInt = screenSize.width;
+	yInt = screenSize.height;
+	
+    //TODO Test it works on iPad, don't know why there are all this cases.
+	
+//    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+//        //its iphone
+//        CGSize result = [[UIScreen mainScreen] bounds].size;
+//        if(result.height == 480) {
+//            // iPhone Classic
+//            xInt = 320;
+//            yInt = 480;
+//        }
+//        else if(result.height == 568) {
+//            // iPhone 5
+//            xInt = 320;
+//            yInt = 568;
+//        }
+//    } else {
+//        //its ipad
+//        xInt = 768;
+//        yInt = 1024;
+//    }
+    
+    UIDeviceOrientation orientation = [[UIDevice currentDevice] orientation];
+    
+    BOOL rotate = YES;
+    
+    switch (orientation) {
+        case UIDeviceOrientationLandscapeLeft:
+            bounds = CGRectMake(0, 0, yInt, xInt);
+            self.captureVideoPreviewLayer.affineTransform = CGAffineTransformMakeRotation(M_PI + M_PI_2); // 270 degress
+            break;
+        case UIDeviceOrientationLandscapeRight:
+            bounds = CGRectMake(0, 0, yInt, xInt);
+            self.captureVideoPreviewLayer.affineTransform = CGAffineTransformMakeRotation(M_PI_2); // 90 degrees
+            break;
+        case UIDeviceOrientationPortraitUpsideDown:
+            rotate = NO;
+            //bounds = CGRectMake(0, 0, xInt, yInt);
+            //self.captureVideoPreviewLayer.affineTransform = CGAffineTransformMakeRotation(M_PI); // 180 degrees
+            break;
+		case UIDeviceOrientationFaceDown:
+		case UIDeviceOrientationFaceUp:
+			rotate = NO;
+			break;
+        default: //Portrait
+            bounds = CGRectMake(0, 0, xInt, yInt);
+            self.captureVideoPreviewLayer.affineTransform = CGAffineTransformMakeRotation(0.0);
+            break;
+    }
+    
+    if (rotate) {
+        self.captureVideoPreviewLayer.position = CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds));
+    }
 }
 
 @end
