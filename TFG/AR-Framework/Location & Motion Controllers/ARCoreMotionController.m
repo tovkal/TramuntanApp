85c0841aaa6374616afa7dc3ce8008d984b6375f
Tovkal <@BTApgI5Dif7fEQ$Go8#Zo8z4n*TsK>
2014-07-27 22:12:37 +0200

    Name changes and readded Settings switches.

--- TFG/AR-Framework/Location & Motion Controllers/ARCoreMotionController.m
+++ TFG/AR-Framework/Location & Motion Controllers/ARCoreMotionController.m
@@ -10,7 +10,7 @@
 
 @interface ARCoreMotionController()
 
-@property (strong, nonatomic) CMMotionManager *coreMotionManager;
+@property (strong, nonatomic) CMMotionManager *motionManager;
 
 @property (strong, nonatomic) NSTimer *timer;
 
@@ -20,28 +20,30 @@
 
 - (void)start
 {
-	self.coreMotionManager = [[CMMotionManager alloc] init];
+	self.motionManager = [[CMMotionManager alloc] init];
 	self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1
 									 target:self
 								   selector:@selector(gotValues)
 								   userInfo:nil
 									repeats:YES];
-	self.coreMotionManager.deviceMotionUpdateInterval = 0.05; //seconds
-	[self.coreMotionManager startDeviceMotionUpdates];
+	self.motionManager.deviceMotionUpdateInterval = 0.05; //seconds
+	//Show calibration HUD when required.
+	self.motionManager.showsDeviceMovementDisplay = YES;
+	[self.motionManager startDeviceMotionUpdatesUsingReferenceFrame:CMAttitudeReferenceFrameXTrueNorthZVertical];
 	
 }
 
 - (void)stop
 {
 	[self.timer invalidate];
-	[self.coreMotionManager stopDeviceMotionUpdates];
-	self.coreMotionManager = nil;
+	[self.motionManager stopDeviceMotionUpdates];
+	self.motionManager = nil;
 }
 
 - (void)gotValues
 {
-	if (self.coreMotionManager.deviceMotion.attitude != nil) {
-		[self.delegate performSelector:@selector(gotNewValues:) withObject:self.coreMotionManager.deviceMotion.attitude];
+	if (self.motionManager.deviceMotion.attitude != nil) {
+		[self.delegate performSelector:@selector(gotNewValues:) withObject:self.motionManager.deviceMotion.attitude];
 	}
 }
 
